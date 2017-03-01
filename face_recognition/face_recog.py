7497e9416694cef186e519b0b2cd3178e1a08e20
Jishnu R <jishnu47@gmail.com>
2017-03-01 23:38:07 +0530

    Changes in Face Recognition Module 1. Made some changes to the way of storing recognition data. 2. Now, Updates recognizer instead of Training it. 3. Now handles fresh run. (Recognizer not trained at all) 4. Cleaned up the code.

--- face_recognition/face_recog.py
+++ face_recognition/face_recog.py
@@ -9,7 +9,6 @@ import thread
 
 faces_path = 'faces'
 xml_path = 'xml'
-debug = False
 i=0
 old_name=""
 new_name=""
@@ -18,51 +17,37 @@ greetings = ["Hi", "Hello"]
 neg_resp = ["Sorry, I am learning", "Pardon me, Who are you?"]
 images = []
 labels = []
-
+first_run = True
+nbr_predicted = 0
+conf = 0
 
 #################### FUNCTIONS ####################
 
-def get_images_and_labels(path):
-    image_paths = [os.path.join(faces_path, f) for f in os.listdir(faces_path)]
-    for image_path in image_paths:
-        image_pil = Image.open(image_path).convert('L')
-        image = np.array(image_pil, 'uint8')
-        image = cv2.equalizeHist(image)
-        nbr = os.path.split(image_path)[1].split("_")[0]
-        images.append(image)
-        labels.append(nbr)
-    return images, labels
-
 def load_recognizer_data():
     if [f for f in os.listdir(xml_path) if f.startswith("face_recog_data")] :
+        global first_run, labels
+        first_run = False
         recognizer.load("xml/face_recog_data.xml")
         print "Loaded Recognizer Data."
-        images, labels = get_images_and_labels(faces_path)
-    else:    
-        train_recognizer()
-
-def train_recognizer():
-    images, labels = get_images_and_labels(faces_path)
-    print "Faces added."
-    if images:
-        recognizer.train(images, np.array(range(0,len(labels))))
-        print "Recognizer trained."
-        recognizer.save("xml/face_recog_data.xml")
-        print "Saved Recognizer Data to xml/face_recog_data.xml."        
-    return
+        labels_load = np.array(np.loadtxt(xml_path+"/face_data.xml", unpack=True, dtype=str, ndmin=1))
 
 def update_recognizer(face,name):
-    recognizer.update(np.array(face), [name])
+    global first_run
+    labels.append(name)
+    recognizer.update(np.array([cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)]), np.array(len(labels)-1))
     print "Recognizer updated."
     recognizer.save("xml/face_recog_data.xml")
-    print "Saved Recognizer Data to xml/face_recog_data.xml."        
+    f = open("xml/face_data.xml", "w")
+    np.savetxt(f, np.column_stack([labels]), fmt='%s')
+    print "Saved Recognizer Data to xml\\face_recog_data.xml and xml\\face_data.xml"    
+    first_run = False
     return
 
 
 #################### Start ####################
 
-print "########### the Machine v0.1 ##########\n########### Jishnu Radhakrishnan ##########\n\n"
-inp = input("Start Face Recognition? Y/N : ")
+print "################## the Machine v_alpha_1 ##################\n########### Jishnu Radhakrishnan : Manoj S Nair ###########\n\n"
+inp = input("Start Face Recognition? \"Y\"/\"N\" : ")
 if inp=="Y":
     face_cascade = cv2.CascadeClassifier('xml/haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('xml/haarcascade_eye.xml')
@@ -86,16 +71,15 @@ if inp=="Y":
             eyes = eye_cascade.detectMultiScale(face_gray, minSize=(w/5, w/5), maxSize=(w/3, w/3))
             for (ex,ey,ew,eh) in eyes:
                 cv2.rectangle(img_shw[y:y+h, x:x+w],(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
-            #cropd = face_crop.CropFace(img, eye_left=(eye_marks[0],eye_marks[1]), eye_right=(eye_marks[2],eye_marks[3]), offset_pct=(0.2,0.2))
-            #cv2.imshow('Face',cropd) 
-            nbr_predicted, conf = recognizer.predict(face_gray)
-            print "Log : Predicted "+labels[nbr_predicted]+" with confidence %d" %(conf)                
-            if conf>90:
+            if(first_run == False):
+                nbr_predicted, conf = recognizer.predict(face_gray)
+                print "Predicted "+labels[nbr_predicted]+" with confidence %d" %(conf)                
+            if conf>90 or (first_run == True):
                 cv2.rectangle(img_shw,(x,y),(x+w,y+h),(0,0,255),2)
                 cv2.imshow('Webcam 0',img_shw)                    
                 speech_eng.say("Sorry, I don't know you. Please type your name")
                 print "Sorry, I don't know you. Please type your name"
-                thread.start_new_thread( speech_eng.runAndWait, () )
+                #thread.start_new_thread(speech_eng.runAndWait, ())
                 user = input("Your Name : ")                
                 if user == '' :
                     user="anon"                    
@@ -104,10 +88,8 @@ if inp=="Y":
                 if temp_path:
                     i = int(os.path.split(temp_path[-1])[1].split(".")[0].replace(user+"_","")) + 1
                 file = "faces/"+user+"_%i.jpg" %(i)
-                if debug:
-                    print "Writing image to : "+file
                 print "Face captured."
-                cv2.imwrite(file, img[y:y+h, x:x+w])                
+                cv2.imwrite(file, img[y:y+h, x:x+w])
                 update_recognizer(img[y:y+h, x:x+w], user)
             else:
                 cv2.rectangle(img_shw,(x,y),(x+w,y+h),(0,255,0),2)
@@ -116,9 +98,6 @@ if inp=="Y":
         cv2.imshow('Webcam 0',img_shw)    
         k = cv2.waitKey(50)# Exit if ESC is pressed
         if k == 27:
-            break
-        elif k == 100: # Key 'd'
-            debug = not debug
-            print "Debug : %i" %(debug)           
+            break         
     cap.release()
     cv2.destroyAllWindows()
